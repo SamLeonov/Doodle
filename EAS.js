fd4eda29e3aaeca6ac213f100f3b1c8efbb70661
SamLeonov <75415780+SamLeonov@users.noreply.github.com>
2021-08-31 21:58:00 +0300

    Add files via upload

---
+++ EAS.js
@@ -0,0 +1,19 @@
+const container = document.getElementById('container');
+let count = prompt('How many squares per side?');
+container.style.gridTemplateColumns='repeat('+count+' 1fr);';
+
+for(let i=1;i<=count*count;i++) {
+    let square = document.createElement('div');
+    square.className='square';
+    square.addEventListener('mouseover',()=>{
+        square.className='colored';
+    });
+    container.appendChild(square);
+}
+const clear = document.getElementById('clear');
+clear.addEventListener('click',()=>{
+    const squares=document.getElementsByClassName('colored');
+    for(let i = squares.length-1;i>=0;i--){
+        squares.item(i).className='square';
+    };
+});
